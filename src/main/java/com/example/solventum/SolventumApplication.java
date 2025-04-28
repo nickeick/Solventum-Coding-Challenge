cd60734a3603a067e8339ae86756472a5b77075c
Nick Eickhoff <nickeick@comcast.net>
2025-04-28 18:09:57 -0500

    New Spring application created using Spring initializer

---
+++ src/main/java/com/example/solventum/SolventumApplication.java
@@ -0,0 +1,13 @@
+package com.example.solventum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolventumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolventumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solventum/SolventumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solventum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolventumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
